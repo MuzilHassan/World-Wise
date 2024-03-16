89036c7dd9594ae70c732ff36934a58e4a1a3162
Muziljohn <muzilhassan9@gmail.com>
2024-03-16 18:07:28 +0500

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PageNotFound from "./PageNotFound";
 import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
 import "./index.css";
+import AppLayout from "./pages/AppLayout";
 function App() {
   return (
     <>
@@ -16,6 +17,12 @@ function App() {
           <Route path="/product" element={<Product />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>index</p>} />
+            <Route path="cities" element={<p>List of cities</p>} />
+            <Route path="countries" element={<p>List of countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
